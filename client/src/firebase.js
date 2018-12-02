20d72f6a49ece67a2f0ecdfa58becf6f4a9b32a3
starcandy <wlziegenfus5@gmail.com>
2018-12-02 00:51:43 +0000

    Updated firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,7 @@
 import firebase from 'firebase'
 
   var config = {
-    apiKey: "AIzaSyAgVtPmMElvVaG33Rf7lXw6J3h02lui0F0",
-    authDomain: "datboi-coin.firebaseapp.com",
-    databaseURL: "https://datboi-coin.firebaseio.com",
-    projectId: "datboi-coin",
-    storageBucket: "",
-    messagingSenderId: "1044269580151"
-  };
+ };
   firebase.initializeApp(config);
 export const  provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
